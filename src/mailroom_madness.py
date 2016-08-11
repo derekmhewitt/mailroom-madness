ab767817594546acb2c6786ba93f84f455f1616e
Justin Lange <well1912@gmail.com>
2016-08-11 16:38:07 -0700

    Implement table printing code

--- src/mailroom_madness.py
+++ src/mailroom_madness.py
@@ -68,3 +68,45 @@ def send_thank_you(donor_data):
     name = prompt_donor_name(donor_data)
     amount = get_int()
     log_thank_you(name, amount, donor_data)
+
+
+def align_cell(value, padding):
+    """Align a single value using padding."""
+    whitespace = ' ' * (padding - len(str(value)) + 2)
+    return '{}{}'.format(value, whitespace)
+
+
+def max_size(t):
+    ''' Return the size of largest sized value from t by its string
+    representation '''
+    return max(map(lambda x: len(str(x)), t))
+
+
+def get_paddings(rows):
+    """Get the whitespace padding amount per column."""
+    return [max_size(map(lambda t: t[i], rows)) for i in range(len(rows[0]))]
+
+
+def print_row(row, padding):
+    for value, padding in zip(row, padding):
+        sys.stdout.write(align_cell(value, padding))
+    sys.stdout.write('\n')
+
+
+def print_table(rows):
+    paddings = get_paddings(rows)
+    for row in rows:
+        print_row(row, paddings)
+
+
+def generate_row(name, donations):
+    total = sum(donations)
+    donation_count = len(donations)
+    average = total/donation_count
+    return (name, total, donation_count, average)
+
+
+def generate_rows(donor_data):
+    t = list(map(lambda k: generate_row(k, donor_data[k]), donor_data.keys()))
+    t.sort(key=lambda x: x[1], reverse=True)
+    return t
